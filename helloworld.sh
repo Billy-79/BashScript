a6744310b908937c1a411f0dc82e113a436ce526
Billy <billy@gmail.com>
2022-12-26 19:27:43 +0200

    Lesson 03 Variables - Updated 02

--- helloworld.sh
+++ helloworld.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-# Lesson 02 - Hello World! #
+### Lesson 02 - Hello World! ###
 #echo "Hello World!"
 
 #echo "My current working directory is:"
 #pwd
 
-# Lesson 03 - Variables #
+### Lesson 03 - Variables ###
 #myname="Billy"
 #myage="43"
 
 #echo "Hello, my name is $myname."
 #echo "I'm $myage years old."
 
-word="fun"
+#word="fun"
 
-echo "Linux is $word"
-echo "Video games are $word"
-echo "Sunny days are $word"
+#echo "Linux is $word"
+#echo "Video games are $word"
+#echo "Sunny days are $word"
+
+now=$(date)
+
+echo "The system time and date is:"
+
+echo $now
