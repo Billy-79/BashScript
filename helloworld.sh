531d50c17f94f442e4ca8daf1b3a757b9c5e39e6
Billy <billy@gmail.com>
2022-12-26 19:15:14 +0200

    Lesson 03 Variables - Updated

--- helloworld.sh
+++ helloworld.sh
@@ -7,8 +7,14 @@
 #pwd
 
 # Lesson 03 - Variables #
-myname="Billy"
-myage="43"
+#myname="Billy"
+#myage="43"
 
-echo "Hello, my name is $myname."
-echo "I'm $myage years old."
+#echo "Hello, my name is $myname."
+#echo "I'm $myage years old."
+
+word="fun"
+
+echo "Linux is $word"
+echo "Video games are $word"
+echo "Sunny days are $word"
