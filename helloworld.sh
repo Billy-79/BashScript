e2712262a2f9dc0e38495f27607684485ac7b8ef
Billy <billy@gmail.com>
2022-12-27 12:37:44 +0200

    Lesson 05 If Statements - Updated 07

--- helloworld.sh
+++ helloworld.sh
@@ -115,9 +115,20 @@
 
 #------------------------------------------------------------------------------------
 
-mynum=300
+#mynum=300
 
-if [ $mynum -ne 200 ]
+#if [ $mynum -ne 200 ]
+#then
+#    echo "The condition is true."
+#else
+#    echo "The variable does not equal 200."
+#fi
+
+#------------------------------------------------------------------------------------
+
+mynum=300
+ 
+if [ $mynum -gt 200 ]
 then
     echo "The condition is true."
 else
