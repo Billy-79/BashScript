f77ca1a8f6ba0bcb5c3720d83bf86d93db1c4a31
Billy <billy@gmail.com>
2022-12-27 20:16:18 +0200

    Lesson 06 Exit Codes - Update 03

--- helloworld.sh
+++ helloworld.sh
@@ -200,11 +200,26 @@
 
 #------------------------------------------------------------------------------------
 
-package=htop
+#package=htop
+
+#sudo apt install $package
+
+#if [ $? -eq 0 ]
+#then
+#    echo "The installation of $package was successful."
+#    echo "The new command is available here:"
+#    which $package
+#else
+#    echo "$package failed to install."
+#fi
+
+#------------------------------------------------------------------------------------
+
+package=notexist
 
 sudo apt install $package
 
-if [ $? -eq 0 ]
+if [ $? -eq 0 ] 
 then
     echo "The installation of $package was successful."
     echo "The new command is available here:"
