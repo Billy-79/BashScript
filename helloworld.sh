f708ed3b3c33666a17a33b3a76fb9bfe26788e65
Billy <billy@gmail.com>
2022-12-27 20:10:44 +0200

    Lesson 06 Exit Codes - Update 02

--- helloworld.sh
+++ helloworld.sh
@@ -192,8 +192,23 @@
 
 #------------------------------------------------------------------------------------
 
-package=notexist
+#package=notexist
  
+#sudo apt install $package
+
+#echo "The exit code for the package install is: $?"
+
+#------------------------------------------------------------------------------------
+
+package=htop
+
 sudo apt install $package
 
-echo "The exit code for the package install is: $?"
+if [ $? -eq 0 ]
+then
+    echo "The installation of $package was successful."
+    echo "The new command is available here:"
+    which $package
+else
+    echo "$package failed to install."
+fi
