8bbedb6134685ef3a1ed5ae2c48c4b57dcb1b947
Billy <billy@gmail.com>
2022-12-28 19:02:45 +0200

    Lesson 06 Exit Codes - Update 06

--- helloworld.sh
+++ helloworld.sh
@@ -245,15 +245,28 @@
 
 #------------------------------------------------------------------------------------
 
-package=notexist
+#package=notexist
 
-sudo apt install $package >> package_install_results.log
+#sudo apt install $package >> package_install_results.log
 
-if [ $? -eq 0 ] 
+#if [ $? -eq 0 ] 
+#then
+#    echo "The installation of $package was successful."
+#    echo "The new command is available here:"
+#    which $package
+#else
+#    echo "$package failed to install." >> package_install_failure.log
+#fi
+
+#------------------------------------------------------------------------------------
+
+directory=/etc
+
+if [ -d $directory ]
 then
-    echo "The installation of $package was successful."
-    echo "The new command is available here:"
-    which $package
+    echo "The directory $directory exists."
 else
-    echo "$package failed to install." >> package_install_failure.log
+    echo "The directory $directory doesn't exist."
 fi
+
+echo "The exit code for this script run is $?"
