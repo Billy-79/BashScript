8a1be43406d971647529a7a0605c9ed3560d834b
Billy <billy@gmail.com>
2022-12-28 20:03:48 +0200

    Lesson 06 While Loops - Initial Commit

--- helloworld.sh
+++ helloworld.sh
@@ -324,17 +324,27 @@
 
 #------------------------------------------------------------------------------------
 
-directory=/etc
+#directory=/etc
 
-if [ -d $directory ]
-then
-    echo "The directory $directory exists."
-    exit 0
-else
-    echo "The directory $directory doesn't exist."
-    exit 1
-fi
+#if [ -d $directory ]
+#then
+#    echo "The directory $directory exists."
+#    exit 0
+#else
+#    echo "The directory $directory doesn't exist."
+#    exit 1
+#fi
 
-echo "The exit code for this script run is $?"
-echo "You didn't see that statement"
-echo "You won't see this one either"
+#echo "The exit code for this script run is $?"
+#echo "You didn't see that statement"
+#echo "You won't see this one either"
+
+#################### Lesson 07 While Loops ####################
+myvar=1
+
+while [ $myvar -le 10 ]
+do
+    echo $myvar
+    myvar=$(( $myvar +1 ))
+    sleep 0.5
+done
