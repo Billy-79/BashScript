a7e4047cb815112c112080794d1e8e5a465f67ec
Billy <billy@gmail.com>
2022-12-28 19:49:49 +0200

    Lesson 06 Exit Codes - Update 13

--- helloworld.sh
+++ helloworld.sh
@@ -318,6 +318,23 @@
 
 #------------------------------------------------------------------------------------
 
-sudo apt install notexist
-exit 0
-echo $?
+#sudo apt install notexist
+#exit 0
+#echo $?
+
+#------------------------------------------------------------------------------------
+
+directory=/etc
+
+if [ -d $directory ]
+then
+    echo "The directory $directory exists."
+    exit 0
+else
+    echo "The directory $directory doesn't exist."
+    exit 1
+fi
+
+echo "The exit code for this script run is $?"
+echo "You didn't see that statement"
+echo "You won't see this one either"
