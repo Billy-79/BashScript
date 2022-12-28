178c0b4f8e113d2410cff0bafc8cfa1b0dee5ce5
Billy <billy@gmail.com>
2022-12-28 19:14:40 +0200

    Lesson 06 Exit Codes - Update 08

--- helloworld.sh
+++ helloworld.sh
@@ -273,12 +273,27 @@
 
 #------------------------------------------------------------------------------------
 
+#directory=/notexist
+
+#if [ -d $directory ]
+#then
+#    echo "The directory $directory exists."
+#else
+#    echo "The directory $directory doesn't exist."
+#fi
+
+#echo "The exit code for this script run is $?"
+
+#------------------------------------------------------------------------------------
+
 directory=/notexist
 
 if [ -d $directory ]
 then
+    echo $?
     echo "The directory $directory exists."
 else
+    echo $?
     echo "The directory $directory doesn't exist."
 fi
 
