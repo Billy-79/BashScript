892b4a787538a44cb9ac18692f72db0338d9640b
Billy <billy@gmail.com>
2022-12-29 20:00:01 +0200

    Lesson 09 For Loops - Updated 01

--- helloworld.sh
+++ helloworld.sh
@@ -426,10 +426,20 @@
 #fi
 
 #################### Lesson 09 For Loops ####################
-for current_number in 1 2 3 4 5 6 7 8 9 10
+#for current_number in 1 2 3 4 5 6 7 8 9 10
+#do 
+#    echo $current_number
+#    sleep 1
+#done
+
+#echo "This is outside of the for loop"
+
+#------------------------------------------------------------------------------------
+
+for current_number in {1..10}
 do 
     echo $current_number
-    sleep 1
+    sleep 0.5
 done
 
 echo "This is outside of the for loop"
