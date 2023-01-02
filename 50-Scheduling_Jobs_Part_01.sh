ac511f72e81e02eaae00516c008db7f9a443dd1d
Billy <billy@gmail.com>
2023-01-02 20:28:04 +0200

    Lesson 14 & 15 Scheduling Jobs - Initial Commit

---
+++ 50-Scheduling_Jobs_Part_01.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#################### Lesson 14 Scheduling Jobs (Part 01) ####################
+
+logfile=job_results.log
+
+echo "The script ran at the following time: $(date)" >> $logfile
---
+++ 51-Scheduling_Jobs_Part_02.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#################### Lesson 15 Scheduling Jobs (Part 02) ####################
+
+logfile=/home/billy/Working-Directory/BashScript/job_results.log
+
+/usr/bin/echo "The script ran at the following time: $(/usr/bin/date)" >> $logfile
