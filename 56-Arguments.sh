d7cff4878a21d5661517ef68003a4d61aff2172f
Billy <billy@gmail.com>
2023-01-03 19:48:53 +0200

    Lesson 16 Arguments - Updated 03

--- 56-Arguments.sh
+++ 56-Arguments.sh
@@ -2,3 +2,13 @@
 
 #################### Lesson 16 Arguments ####################
 
+lines=$(ls -lha $1 | wc -l)
+
+if [ $# -ne 1 ]
+then
+    echo "This script requires exactly one directory path passed to it."
+    echo "Please try again."
+    exit 1
+fi
+
+echo "You have $(($lines-3)) objects in the $1 directory."
